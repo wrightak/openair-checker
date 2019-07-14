71349d4e122f981046ae88ca20e149b65873a22d
Andrew Wright <wrightak@gmail.com>
2019-07-14 17:07:17 +0900

    Initial commit

---
+++ contentScript.js
@@ -0,0 +1,36 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+const previousMonday = moment().day() >= 0 ? moment().day(-6) : moment.day(1);
+const previousTuesday = moment().day() >= 0 ? moment().day(-5) : moment.day(2);
+const previousWednesday = moment().day() >= 0 ? moment().day(-4) : moment.day(3);
+const previousThursday = moment().day() >= 0 ? moment().day(-3) : moment.day(4);
+const previousFriday = moment().day() >= 0 ? moment().day(-2) : moment.day(5);
+
+const expectedDates = [
+    previousMonday.format("MM/DD/YYYY"),
+    previousTuesday.format("MM/DD/YYYY"),
+    previousWednesday.format("MM/DD/YYYY"),
+    previousThursday.format("MM/DD/YYYY"),
+    previousFriday.format("MM/DD/YYYY")
+];
+
+const xPathResult = document.evaluate(
+    '//td[@align="right"][contains(text(),"2019")]',
+    document,
+    null,
+    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+    null
+);
+
+for (let i = 0; i < xPathResult.snapshotLength; i++) {
+    const element = xPathResult.snapshotItem(i);
+    if (expectedDates.includes(element.textContent)) {
+        element.setAttribute('class', 'expected-date');
+    } else {
+        element.setAttribute('class', 'unexpected-date');
+    }
+}
