b3d93f9f0f32e7ce9159c7b0f68e66558336fab8
Andrew Wright <wrightak@gmail.com>
2019-07-19 16:01:14 +0900

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,10 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-const previousMonday = moment().day() < 5 ? moment().day(-6) : moment.day(1);
-const previousTuesday = moment().day() < 5 ? moment().day(-5) : moment.day(2);
-const previousWednesday = moment().day() < 5 ? moment().day(-4) : moment.day(3);
-const previousThursday = moment().day() < 5 ? moment().day(-3) : moment.day(4);
-const previousFriday = moment().day() < 5 ? moment().day(-2) : moment.day(5);
+const previousMonday = moment().day() < 5 ? moment().day(-6) : moment().day(1);
+const previousTuesday = moment().day() < 5 ? moment().day(-5) : moment().day(2);
+const previousWednesday = moment().day() < 5 ? moment().day(-4) : moment().day(3);
+const previousThursday = moment().day() < 5 ? moment().day(-3) : moment().day(4);
+const previousFriday = moment().day() < 5 ? moment().day(-2) : moment().day(5);
 
 const expectedDates = [
     previousMonday.format("MM/DD/YYYY"),
