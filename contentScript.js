853b7b9ddbe554688e914a0812d9fd89a0db24a5
Andrew Wright <wrightak@gmail.com>
2019-07-20 13:33:37 +0900

    Check for submitted timesheets using page content rather than URL

--- contentScript.js
+++ contentScript.js
@@ -1,61 +1,75 @@
 'use strict';
 
-const currentYear = moment().year();
-const dateCellXPath = '//td[@align="right"][contains(text(),"' + currentYear + '")]';
-const dateCellXpathResult = document.evaluate(
-    dateCellXPath,
+const isSubmittedTimesheet = document.evaluate(
+    '//td[contains(., "Status: Submitted")][contains(., "Timesheet Report")]',
     document,
     null,
-    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+    XPathResult.BOOLEAN_TYPE,
     null
 );
 
-for (let i = 0; i < dateCellXpathResult.snapshotLength; i++) {
-    const element = dateCellXpathResult.snapshotItem(i);
-
-    const formats = [
-        "MM-DD-YYYY",
-        "MM/DD/YYYY",
-        "MM-DD-YY",
-        "MM/DD/YY",
-        "MMM/DD/YYYY",
-        "DD-MM-YYYY",
-        "DD-MM-YY",
-        "DD.MM.YY",
-        "DD.MM.YYYY",
-        "DD/MM/YY",
-        "DD/MM/YYYY",
-        "YYYY-MM-DD",
-        "YYYY.MM.DD",
-        "DD-MMM-YY",
-        "DD-MMM-YYYY",
-        "YY-MMM-DD",
-        "YYYY-MMM-DD",
-    ];
-
-    let date = moment(element.textContent, formats);
-    if (dateIsExpected(date)) {
-        element.setAttribute('class', 'expected-date');
-    } else {
-        element.setAttribute('class', 'unexpected-date');
-    }
+if (isSubmittedTimesheet.booleanValue) {
+    checkTimesheet()
 }
 
-function dateIsExpected(date) {
-    const previousMonday = moment().day() < 5 ? moment().day(-6) : moment().day(1);
-    const previousTuesday = moment().day() < 5 ? moment().day(-5) : moment().day(2);
-    const previousWednesday = moment().day() < 5 ? moment().day(-4) : moment().day(3);
-    const previousThursday = moment().day() < 5 ? moment().day(-3) : moment().day(4);
-    const previousFriday = moment().day() < 5 ? moment().day(-2) : moment().day(5);
+function checkTimesheet() {
+    const currentYear = moment().year();
+    const dateCellXPath = '//td[@align="right"][contains(text(),"' + currentYear + '")]';
+    const dateCellXpathResult = document.evaluate(
+        dateCellXPath,
+        document,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null
+    );
 
-    const expectedDates = [previousMonday, previousTuesday, previousWednesday, previousThursday, previousFriday];
+    for (let i = 0; i < dateCellXpathResult.snapshotLength; i++) {
+        const element = dateCellXpathResult.snapshotItem(i);
 
-    let isExpected = false;
-    expectedDates.forEach(expectedDate => {
-        if (expectedDate.isSame(date, 'day')) {
-            isExpected = true;
+        const formats = [
+            "MM-DD-YYYY",
+            "MM/DD/YYYY",
+            "MM-DD-YY",
+            "MM/DD/YY",
+            "MMM/DD/YYYY",
+            "DD-MM-YYYY",
+            "DD-MM-YY",
+            "DD.MM.YY",
+            "DD.MM.YYYY",
+            "DD/MM/YY",
+            "DD/MM/YYYY",
+            "YYYY-MM-DD",
+            "YYYY.MM.DD",
+            "DD-MMM-YY",
+            "DD-MMM-YYYY",
+            "YY-MMM-DD",
+            "YYYY-MMM-DD",
+        ];
+
+        let date = moment(element.textContent, formats);
+        if (dateIsExpected(date)) {
+            element.setAttribute('class', 'expected-date');
+        } else {
+            element.setAttribute('class', 'unexpected-date');
         }
-    });
+    }
 
-    return isExpected;
+    function dateIsExpected(date) {
+        const previousMonday = moment().day() < 5 ? moment().day(-6) : moment().day(1);
+        const previousTuesday = moment().day() < 5 ? moment().day(-5) : moment().day(2);
+        const previousWednesday = moment().day() < 5 ? moment().day(-4) : moment().day(3);
+        const previousThursday = moment().day() < 5 ? moment().day(-3) : moment().day(4);
+        const previousFriday = moment().day() < 5 ? moment().day(-2) : moment().day(5);
+
+        const expectedDates = [previousMonday, previousTuesday, previousWednesday, previousThursday, previousFriday];
+
+        let isExpected = false;
+        expectedDates.forEach(expectedDate => {
+            if (expectedDate.isSame(date, 'day')) {
+                isExpected = true;
+            }
+        });
+
+        return isExpected;
+    }
 }
